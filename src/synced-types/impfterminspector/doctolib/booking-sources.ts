a496b9f6ec643e74d1a23d2fdac5517ac8a8013c
Joshua May <notjosh@gmail.com>
2021-06-05 13:03:03 +0200

    Sync types

--- src/synced-types/impfterminspector/doctolib/booking-sources.ts
+++ src/synced-types/impfterminspector/doctolib/booking-sources.ts
@@ -1,4 +1,4 @@
-import { InsuranceType } from './insurance-types';
+import { InsuranceType, insuranceTypes } from './insurance-types';
 import { BookingSource } from './types';
 import * as VaccinationSite from './vaccination-sites';
 import { VaccinationType } from './vaccination-types';
@@ -46,6 +46,14 @@ export const bookingSources: BookingSource[] = [
     insurance: InsuranceType.PRIVATE,
     vaccination: VaccinationType.ASTRAZENECA,
   },
+  {
+    site: VaccinationSite.tegel,
+    insurance: InsuranceType.PUBLIC,
+    vaccination: VaccinationType.MODERNA,
+    override: {
+      agendaIds: ['465555'],
+    },
+  },
 
   {
     site: VaccinationSite.tempelhof,
@@ -56,6 +64,48 @@ export const bookingSources: BookingSource[] = [
     site: VaccinationSite.tempelhof,
     insurance: InsuranceType.PRIVATE,
     vaccination: VaccinationType.ASTRAZENECA,
+  },
+  {
+    site: VaccinationSite.tempelhof,
+    insurance: InsuranceType.PUBLIC,
+    vaccination: VaccinationType.MODERNA,
+    override: {
+      agendaIds: [
+        '467906',
+        '481913',
+        '481915',
+        '481920',
+        '481917',
+        '467934',
+        '467937',
+        '467938',
+        '467939',
+        '467910',
+        '467908',
+        '467903',
+        '467907',
+        '467935',
+        '467936',
+        '467893',
+        '467895',
+        '467896',
+        '467900',
+        '467901',
+        '467905',
+        '467911',
+        '467897',
+        '467898',
+        '467912',
+        '467940',
+        '481914',
+        '481916',
+        '481919',
+        '481921',
+        '467894',
+        '467933',
+        '467899',
+      ],
+    },
   },
 
   {
